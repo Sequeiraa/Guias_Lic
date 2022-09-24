cd19c7aa14c1a88bd37c9c7c810e155e56153a06
José Sequeira <sequeirajosecarlos@gmail.com>
2022-09-23 19:19:45 -0600

    funcionalidad de los archivos html con Js

---
+++ JS/numeroAleatorio.js
@@ -0,0 +1,22 @@
+
+const numeroAleatorio = math.floor(Math.random() * 25) + 1;
+
+const numeroIntentos = 3;
+
+let intentos = 1;
+function generarNumeroAleatorio(){
+
+    let mensaje;
+
+    const parrafo = document.querySelector("#idParrafo");
+
+
+    if(intentos <= numeroIntentos){
+        let numero = prompt(
+            "¿Qué número se ha generado (Intento " + ")?"
+        )
+
+        if(numero)
+    }
+}
+//hola mucho gusto estoy escribiendo con el teclado, probando su uso despues de meses
